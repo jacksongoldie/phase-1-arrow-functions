8a59aad20e1e34797b3a81dc00a7009bef8cd7fe
Goldie <goldiejackson25@yahoo.com>
2021-09-01 06:30:04 -0500

    tests passing

---
+++ index.js
@@ -0,0 +1,11 @@
+//function expression called divide
+const divide = function(){
+    return (2000/100);
+}
+
+//arrow function square
+const square = x => x**2;
+
+
+//arrow function add takes two para
+const add = (x, y) => x + y;
